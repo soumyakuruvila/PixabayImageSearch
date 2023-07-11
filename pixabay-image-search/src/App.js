1f2803ffe00a5e9c73c76f409f89dbe8164433c1
Soumya Kuruvila <soumyasarah@gmail.com>
2023-07-11 16:06:40 -0700

    Displaying images

--- pixabay-image-search/src/App.js
+++ pixabay-image-search/src/App.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import * as Keys from './data/Keys.js';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import "./index.css";
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
   const key = Keys.API_KEY;
@@ -28,11 +40,17 @@ function App() {
         placeholder="Enter search term"
       />
       <button onClick={handleSearch}>Search</button>
-      <ul>
-        {searchResults.map((result) => (
-          <li key={result.id}>{result.pageURL}</li>
-        ))}
-      </ul>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          {searchResults.map((result) => (
+            <Grid item xs={2} sm={4} md={4} key={result.id}>
+              <Item key={result.id}>
+                <img className="photo" src={result.largeImageURL} alt="" />
+              </Item>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </div>
   );
 }
